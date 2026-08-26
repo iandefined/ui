6bed38ab7abc2b353c3bee58525001aca18e05fa
Ian de Jesus <275540602+iandefined@users.noreply.github.com>
2026-08-26 11:06:07 +0800

    fix: font weight of hitbox checkbox

--- examples/hitbox/basic.tsx
+++ examples/hitbox/basic.tsx
@@ -9,7 +9,7 @@ export default function HitboxDemo() {
   return (
     <div className="relative flex min-h-64 w-full flex-col items-center justify-center gap-5">
       <label
-        className="absolute top-2 -right-2 flex items-center gap-2 cursor-pointer text-muted-foreground font-medium"
+        className="absolute top-2 -right-2 flex items-center gap-2 cursor-pointer text-muted-foreground"
         htmlFor="show-hitbox"
       >
         <Checkbox
@@ -23,7 +23,9 @@ export default function HitboxDemo() {
       <Button
         className={`group/hitbox w-30 hitbox-6 ${showHitbox ? "hitbox-debug" : ""}`}
       >
-        <span className="group-hover/hitbox:hidden">hitbox-6</span>
+        <span className="group-hover/hitbox:hidden group-active:hidden">
+          hitbox-6
+        </span>
         <span className="not-group-hover/hitbox:hidden group-active:hidden">
           Hovered
         </span>
