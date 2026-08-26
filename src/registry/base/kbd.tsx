04d76de2a9b49c545aef40fc1f4d14f21b5bd841
Ian de Jesus <275540602+iandefined@users.noreply.github.com>
2026-08-26 21:57:10 +0800

    feat: add bottom stroke to kbd component

--- src/registry/base/kbd.tsx
+++ src/registry/base/kbd.tsx
@@ -4,7 +4,7 @@ const Kbd = ({ className, ...props }: React.ComponentProps<"kbd">) => (
   <kbd
     data-slot="kbd"
     className={cn(
-      "bg-muted text-muted-foreground pointer-events-none inline-flex h-5 w-fit min-w-5 items-center justify-center gap-1 rounded-sm border border-accent border-b-2 px-1 font-sans text-xs font-medium select-none [&_svg:not([class*='size-'])]:size-3",
+      "bg-muted text-muted-foreground pointer-events-none inline-flex h-5 w-fit min-w-5 items-center justify-center gap-1 rounded-sm border border-border border-b-2 px-1 font-sans text-xs font-medium select-none [&_svg:not([class*='size-'])]:size-3",
       "in-data-[slot=tooltip-content]:bg-background/20 in-data-[slot=tooltip-content]:text-background dark:in-data-[slot=tooltip-content]:bg-background/10",
       className
     )}
@@ -12,7 +12,7 @@ const Kbd = ({ className, ...props }: React.ComponentProps<"kbd">) => (
   />
 );
 
-const KbdGroup = ({ className, ...props }: React.ComponentProps<"kbd">) => (
+const KbdGroup = ({ className, ...props }: React.ComponentProps<"div">) => (
   <kbd
     data-slot="kbd-group"
     className={cn("inline-flex items-center gap-1", className)}
